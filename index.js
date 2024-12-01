1b6fa748b470828e27b8deec495b1525e8d6684e
António Eleutério <aleutheris@gmail.com>
2024-12-01 10:45:02 +0100

    Implementing Resolvers

--- index.js
+++ index.js
@@ -1,24 +1,30 @@
 const { ApolloServer, gql } = require('apollo-server');
-
+const sessions = require('./data/sessions.json');
 
 const typeDefs = gql`
-type Query {
-  sessions: [Session]
-}
-type Session {
-  id: ID!
-  title: String!,
-  description: String,
-  startsAt: String,
-  endsAt: String,
-  room: String,
-  day: String,
-  format: String,
-  track: String,
-  level: String
+  type Query {
+    sessions: [Session]
+  }
+  type Session {
+    id: ID!
+    title: String!,
+    description: String,
+    startsAt: String,
+    endsAt: String,
+    room: String,
+    day: String,
+    format: String,
+    track: String,
+    level: String
 }`
 
-const server = new ApolloServer({typeDefs});
+const resolvers = {
+  Query: {
+    sessions: () => sessions
+  }
+}
+
+const server = new ApolloServer({ typeDefs, resolvers });
 
 server
   .listen({port: process.env.PORT || 4000 })
