7344b7715933419746ae227a5f457ae5f2132e79
António Eleutério <aleutheris@gmail.com>
2024-12-06 10:06:04 +0100

    Modularizing Resolvers

--- resolvers.js
+++ resolvers.js
@@ -1,28 +1,7 @@
-const _ = require('lodash');
+const Query = require('./resolvers/query');
+const Session = require('./resolvers/session');
 
 module.exports =  {
-  Query: {
-    sessions: (parents, args, {dataSources}, info) => {
-      return dataSources.sessionAPI.getSessions(args);
-    },
-    sessionById: (parents, {id}, {dataSources}, info) => {
-      return dataSources.sessionAPI.getSessionById(id);
-    },
-    speakers: (parents, args, {dataSources}, info) => {
-      return dataSources.speakerAPI.getSpeakers();
-    },
-    speakerById: (parents, {id}, {dataSources}, info) => {
-      return dataSources.speakerAPI.getSpeakerById(id);
-    },
-  },
-  Session: {
-    async speakers(session, agrs, {dataSources}) {
-      const speakers = await dataSources.speakerAPI.getSpeakers();
-      const returns = speakers.filter(speaker => {
-        return _.filter(session.speakers, {id: speaker.id}).length > 0;
-      });
-
-      return returns;
-    },
-  },
+  Query,
+  Session,
 };
