5bd357f0daadc53fad814d925f935c4fc2ddb997
António Eleutério <aleutheris@gmail.com>
2024-12-10 08:51:06 +0100

    Throwing Errors

--- resolvers/session.js
+++ resolvers/session.js
@@ -1,12 +1,18 @@
+const { ApolloError } = require('apollo-server');
 const _ = require('lodash');
 
 module.exports =  {
-  async speakers(session, agrs, {dataSources}) {
-    const speakers = await dataSources.speakerAPI.getSpeakers();
-    const returns = speakers.filter(speaker => {
-      return _.filter(session.speakers, {id: speaker.id}).length > 0;
-    });
-
-    return returns;
+  speakers: async (session, agrs, {dataSources}, info) => {
+    try {
+      const speakers = await dataSources.speakerAPI.getSpeakers();
+      const returns = speakers.filter(speaker => {
+        return _.filter(session.speakers, {id: speaker.id}).length > 0;
+      });
+      return returns;
+    } catch (error) {
+      return new ApolloError("Unable to get speakers", "SPEAKERAPIERROR", {
+        token: "UNIQUETOKEN"
+      });
+    }
   },
 }
